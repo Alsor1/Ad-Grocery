9e1d1856ab89d97613a90e06882638d1169cec49
LepinzeanStefan <stefan.lepinzean@gmail.com>
2024-12-07 22:02:32 +0200

    Added the grocery list 1.2 with bugs

--- app/src/main/java/com/example/ad_grocery/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/ad_grocery/ui/gallery/GalleryFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ad_grocery.databinding.FragmentGalleryBinding
 import com.example.ad_grocery.objects.Produce
