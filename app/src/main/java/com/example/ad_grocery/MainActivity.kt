e406950c16868b8d9b7f64524f09d6cedbeaa935
Alsor1 <alsor469@gmail.com>
2024-12-08 00:27:08 +0200

    Populated the database a bit

--- app/src/main/java/com/example/ad_grocery/MainActivity.kt
+++ app/src/main/java/com/example/ad_grocery/MainActivity.kt
@@ -12,9 +12,10 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ad_grocery.databinding.ActivityMainBinding
+import com.example.ad_grocery.objects.ProductDB
+import com.example.ad_grocery.objects.Produce
 import com.example.ad_grocery.objects.User
-import java.util.Date
-import java.util.HashMap
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,19 +53,19 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_grocery, R.id.nav_profile, R.id.nav_history
+                R.id.nav_home, R.id.nav_grocery, R.id.nav_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Initialize the hardcoded database
+        initializeDatabase()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
@@ -72,6 +73,36 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun initializeDatabase() {
+        // Add products to the database
+        ProductDB.addProductType(
+            "Water",
+            listOf(
+                Produce("1", 100, Date(System.currentTimeMillis() + 86400000L * 100), 2.5f, 50, 1, 0.1f, "apa1.jpg"),
+                Produce("2", 101, Date(System.currentTimeMillis() + 86400000L * 70), 3.0f, 30, 1, 0.05f, "apa2.jpg"),
+                Produce("3", 102, Date(System.currentTimeMillis() + 86400000L * 150), 4.0f, 20, 1, 0.15f, "apa3.jpg")
+            )
+        )
+
+        ProductDB.addProductType(
+            "Chocolate_Bar",
+            listOf(
+                Produce("4", 201, Date(System.currentTimeMillis() + 86400000L * 20), 10.5f, 100, 2, 0.0f, "baton1.jpg"),
+                Produce("5", 202, Date(System.currentTimeMillis() + 86400000L * 30), 15.0f, 80, 2, 0.1f, "baton2.jpg"),
+                Produce("6", 203, Date(System.currentTimeMillis() + 86400000L * 40), 20.0f, 85, 2, 0.15f, "baton3.jpg")
+            )
+        )
+
+        ProductDB.addProductType(
+            "Notebook",
+            listOf(
+                Produce("7", 301, Date(System.currentTimeMillis() + 86400000L * 20), 5.0f, 60, 3, 0.2f, "caiet1.jpg"),
+                Produce("8", 302, Date(System.currentTimeMillis() + 86400000L * 18), 4.5f, 40, 3, 0.3f, "caiet2.jpg"),
+                Produce("8", 303, Date(System.currentTimeMillis() + 86400000L * 18), 8.5f, 70, 3, 0.1f, "caiet3.jpg")
+            )
+        )
     }
 
     fun getBudget(): Float {
--- app/src/main/java/com/example/ad_grocery/objects/Produce.kt
+++ app/src/main/java/com/example/ad_grocery/objects/Produce.kt
@@ -1,128 +1,128 @@
-package com.example.ad_grocery.objects
+    package com.example.ad_grocery.objects
 
-import java.util.Date
+    import java.util.Date
 
-class IdGenerator() {
-    private var lastId = 0
+    class IdGenerator() {
+        private var lastId = 0
 
-    fun getId(): Int {
-        return ++lastId
-    }
-}
-
-object ProductDB {
-    private val productDatabase: HashMap<String, MutableList<Produce>> = HashMap()
-
-    fun addProductType(productType: String, prices: List<Produce>) {
-        if (productType in productDatabase) {
-            productDatabase[productType]?.addAll(prices)
-        } else {
-            productDatabase[productType] = prices.toMutableList()
+        fun getId(): Int {
+            return ++lastId
         }
     }
 
-    fun addPriceToProduct(productType: String, product: Produce) {
-        productDatabase.computeIfAbsent(productType) { mutableListOf() }.add(product)
-    }
+    object ProductDB {
+        private val productDatabase: HashMap<String, MutableList<Produce>> = HashMap()
 
-    fun getProducts(productType: String): List<Produce>? {
-        return productDatabase[productType]
-    }
-
-    fun removeProductType(productType: String) {
-        productDatabase.remove(productType)
-    }
-
-    fun removePriceFromProduct(productType: String, produce: Produce) {
-        productDatabase[productType]?.remove(produce)
-    }
-
-    fun hasProductType(productType: String): Boolean {
-        return productType in productDatabase
-    }
-
-    fun getAllProductTypes(): Set<String> {
-        return productDatabase.keys
-    }
-
-    fun getDatabase(): HashMap<String, MutableList<Produce>> {
-        return productDatabase
-    }
-
-    fun calculateCostScore(id: String, brand: Int): Int {
-        val prod: List<Produce>? = this.getProducts(id)
-    
-        if (prod.isNullOrEmpty()) {
-            return 0
-        }
-    
-        var isBrandPresent = false
-        for (p in prod) {
-            if (p.brand == brand) {
-                isBrandPresent = true
-                break
+        fun addProductType(productType: String, prices: List<Produce>) {
+            if (productType in productDatabase) {
+                productDatabase[productType]?.addAll(prices)
+            } else {
+                productDatabase[productType] = prices.toMutableList()
             }
         }
-    
-        if (!isBrandPresent) {
-            return 0
+
+        fun addPriceToProduct(productType: String, product: Produce) {
+            productDatabase.computeIfAbsent(productType) { mutableListOf() }.add(product)
         }
-    
-        val top10Products = prod.sortedBy { it.cost }.take(10)
-    
-        for ((index, p) in top10Products.withIndex()) {
-            if (p.brand == brand) {
-                return 10 - index
+
+        fun getProducts(productType: String): List<Produce>? {
+            return productDatabase[productType]
+        }
+
+        fun removeProductType(productType: String) {
+            productDatabase.remove(productType)
+        }
+
+        fun removePriceFromProduct(productType: String, produce: Produce) {
+            productDatabase[productType]?.remove(produce)
+        }
+
+        fun hasProductType(productType: String): Boolean {
+            return productType in productDatabase
+        }
+
+        fun getAllProductTypes(): Set<String> {
+            return productDatabase.keys
+        }
+
+        fun getDatabase(): HashMap<String, MutableList<Produce>> {
+            return productDatabase
+        }
+
+        fun calculateCostScore(id: String, brand: Int): Int {
+            val prod: List<Produce>? = this.getProducts(id)
+
+            if (prod.isNullOrEmpty()) {
+                return 0
             }
-        }
-    
-        return 0
-    }
-    
-}
 
-class Produce(
-    val id: String,
-    val brand: Int,
-    val expiry: Date,
-    val cost: Float,
-    var quantity: Int,
-    var category: Int,
-    var discount: Float,
-    val imageAddress: String
-) {
-    var utility: Float = 0f
-
-    fun calculateUtility(user: User): Float {
-        val prefWeight = 0.5f
-        val costWeight = 0.3f
-        val expiryWeight = 0.2f
-        val preferenceScore = if (user.preferences[this] == true) 1 else 0
-        val expiryScore = calculateExpiryScore()
-        val costScore = ProductDB.calculateCostScore(this.id, this.brand)
-
-        return prefWeight * preferenceScore + costWeight * costScore + expiryWeight * expiryScore + discount
-    }
-
-    fun calculateDiscountedCost(): Float {
-        return cost * (1 - discount)
-    }
-
-    fun calculateExpiryScore(): Int {
-        val expiryDate = expiry.time
-        val currentDate = Date().time
-        val timeDifference = expiryDate - currentDate
-        val daysDifference = (timeDifference / (1000 * 60 * 60 * 24)).toInt()
-
-        if (daysDifference < 1) {
-            return 0
-        } else {
-            for (i in 1..10) {
-                if (daysDifference < 3 * i) {
-                    return i
+            var isBrandPresent = false
+            for (p in prod) {
+                if (p.brand == brand) {
+                    isBrandPresent = true
+                    break
                 }
             }
+
+            if (!isBrandPresent) {
+                return 0
+            }
+
+            val top10Products = prod.sortedBy { it.cost }.take(10)
+
+            for ((index, p) in top10Products.withIndex()) {
+                if (p.brand == brand) {
+                    return 10 - index
+                }
+            }
+
+            return 0
         }
-        return 10
+
     }
-}
+
+    class Produce(
+        val id: String,
+        val brand: Int,
+        val expiry: Date,
+        val cost: Float,
+        var quantity: Int,
+        var category: Int,
+        var discount: Float,
+        val imageAddress: String
+    ) {
+        var utility: Float = 0f
+
+        fun calculateUtility(user: User): Float {
+            val prefWeight = 0.5f
+            val costWeight = 0.3f
+            val expiryWeight = 0.2f
+            val preferenceScore = if (user.preferences[this] == true) 1 else 0
+            val expiryScore = calculateExpiryScore()
+            val costScore = ProductDB.calculateCostScore(this.id, this.brand)
+
+            return prefWeight * preferenceScore + costWeight * costScore + expiryWeight * expiryScore + discount
+        }
+
+        fun calculateDiscountedCost(): Float {
+            return cost * (1 - discount)
+        }
+
+        fun calculateExpiryScore(): Int {
+            val expiryDate = expiry.time
+            val currentDate = Date().time
+            val timeDifference = expiryDate - currentDate
+            val daysDifference = (timeDifference / (1000 * 60 * 60 * 24)).toInt()
+
+            if (daysDifference < 1) {
+                return 0
+            } else {
+                for (i in 1..10) {
+                    if (daysDifference < 3 * i) {
+                        return i
+                    }
+                }
+            }
+            return 10
+        }
+    }
