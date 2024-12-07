4b33140991eef3532152ddb5e2bb1a20eff0d9e6
LepinzeanStefan <stefan.lepinzean@gmail.com>
2024-12-07 16:45:00 +0200

    Added some functionalities for user

--- app/src/main/java/com/example/ad_grocery/objects/User.kt
+++ app/src/main/java/com/example/ad_grocery/objects/User.kt
@@ -1,14 +1,32 @@
 package com.example.ad_grocery.objects
 
-import java.util.ArrayList
 import java.util.Date
 import java.util.HashMap
+import kotlin.collections.ArrayList
 
 class User(
-    val id: String, var name: String, var initBudget: Float,
+    val id: Int, var name: String, var initBudget: Float,
     var currBudget: Float, var daysInterval: Int, var lastReset: Date,
-    var preferences: HashMap<Produce, Int>, var history: ArrayList<Produce>,
+    var preferences: HashMap<String, Int>, var history: ArrayList<Produce>,
     var toBuy: ArrayList<Produce>
 ) {
+    fun resetBudget(currTime: Date): Boolean {
+        val timePassed = (currTime.time - lastReset.time) / 86400000
+        if (timePassed >= daysInterval) {
+            currBudget = initBudget
+            return true
+        }
+        return false
+    }
 
+    fun boughtList() {
+        for (prod in toBuy) {
+            history.add(prod)
+        }
+        toBuy = ArrayList()
+    }
+
+    fun setPreference(id: String, level: Int) {
+        preferences[id] = level
+    }
 }
